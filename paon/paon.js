ea7241053b258993c5fb9708e8cb9d1025f79db1
Bichon Codeurs <jeremie.meunier74@gmail.com>
2023-02-12 20:44:10 +0100

    Adding paon

---
+++ paon/paon.js
@@ -0,0 +1,56 @@
+function PaonSelect_Initialize() {
+	var arr_paon_select = document.querySelectorAll('[data-paon-select]');
+	for(var i = 0;i < arr_paon_select.length; i++) {
+		PaonSelect_Worker(arr_paon_select[i]);
+
+        var paon_value = arr_paon_select[i].querySelector('[data-paon-value]');
+        var paon_selected = arr_paon_select[i].querySelector('ul li.paon--selected');
+        var paon_selected_value = paon_selected.innerText;
+
+        paon_value.innerText = paon_selected_value;
+	}
+}
+
+function PaonSelect_Worker(item) {
+    var paon_view = item.querySelector('[data-paon-view]');
+    var arr_paon_options = item.querySelectorAll('li.paon--option');
+    var paon_list = item.querySelector('ul');
+
+    paon_list.addEventListener('mouseleave', function(e) {
+        var block = e.target;
+        
+        block.parentNode.classList.remove('paon--list-active');
+        block.parentNode.classList.add('paon--list-inactive');
+    });
+
+	paon_view.addEventListener('click', function(e) {
+		e.preventDefault();
+		
+		if(item.classList.contains('paon--list-inactive')) {
+			item.classList.remove('paon--list-inactive');
+			item.classList.add('paon--list-active');
+		}
+		else {
+			item.classList.remove('paon--list-active');
+			item.classList.add('paon--list-inactive');
+		}
+	});
+
+    for(var i = 0;i < arr_paon_options.length; i++) {
+        if(!arr_paon_options[i].classList.contains('paon--selected')) {
+            arr_paon_options[i].addEventListener('click', function(e) {
+                var list = e.srcElement.offsetParent;
+                var view = list.parentNode.querySelector('[data-paon-value]');
+                var selected = list.querySelector('li.paon--selected');
+                var option = e.target;
+                var value = option.innerText;
+
+                selected.classList.remove('paon--selected');
+                option.classList.add('paon--selected');
+                view.innerText = value;
+
+                PaonFunctionBridge(e);
+            });
+        }
+    }
+}
