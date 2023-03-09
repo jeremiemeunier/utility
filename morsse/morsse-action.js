b413fc1fae3cd5b6e6814312cf45b5f435c594cf
Bichon Codeurs <jeremie.meunier74@gmail.com>
2023-03-09 10:52:13 +0100

    Create morsse-action.js

---
+++ morsse/morsse-action.js
@@ -0,0 +1,32 @@
+function MorsseAction_Initialize() {
+    var arr_actionner = document.querySelectorAll('[data-morsse-action]');
+    for(var i = 0;i < arr_actionner.length;i++) {
+        arr_actionner[i].addEventListener('click', MorsseAction_Worker);
+    }
+
+    var arr_form = document.querySelectorAll('form[data-morsse-action-form]');
+    for(var i = 0;i < arr_form.length;i++) {
+        arr_form[i].addEventListener('submit', function(e) {
+            e.preventDefault();
+        })
+    }
+}
+
+function MorsseAction_Worker(e) {
+    e.preventDefault();
+
+    var arr_form = document.querySelectorAll('form[data-morsse-action-form]');
+
+    for(var i = 0;i < arr_form.length;i++) {
+        let actionForm_xhr = new XMLHttpRequest();
+        let actionUrl = arr_form[i].getAttribute('action');
+        let actionForm_formData = new FormData(arr_form[i]);
+
+        actionForm_xhr.open('POST', actionUrl, true);
+        actionForm_xhr.send(actionForm_formData);
+    }
+
+    window.setTimeout(function() {
+        location.reload();
+    }, 200);
+}
